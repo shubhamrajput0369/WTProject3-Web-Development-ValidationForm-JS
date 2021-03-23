bf751d02a1f4966d8e6576c699372fdb4586ce2f
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-23 21:09:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ $(document).ready(function () {
     jQuery.validator.addMethod("custom", function (value, element) {
         return /^[a-zA-Z]+$/.test(value);
     }, "* Please enter something");
-
-   
+ $('form[id="regs"]').validate({
+        rules: {
+            fname: {
+                required: true,
+                custom: true,
+            },
+            lname: {
+                required: true,
+                custom: true,
+            },
