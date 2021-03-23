f9b85d2823e55649369294ccff2731503743b7d7
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-23 21:12:02 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//Author: Shubham Bhagwansing Rajput
+//JS
+
+function validate(){
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var error_message = document.getElementById("error_message");
+  
